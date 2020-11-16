1890ccbf45a7e910b173cc11f8747020f6b359fb
nemka88 <alisapavlova1920@gmail.com>
2020-11-16 10:57:32 +0300

    задание от 16.11

---
+++ факториалы 2.cpp
@@ -0,0 +1,34 @@
+﻿// факториалы.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N{ 0 };
+
+
+    auto fact = [](auto& self, int N)
+    {
+        if (N < 0) 
+        {
+            return 0; 
+        }
+        else if (N == 0) 
+        {
+            return 1; 
+        }
+        else 
+        {
+            return N * self(self, N - 1); 
+        }
+    };
+
+    cout << "Введите факториал" << endl;
+    cin >> N;
+    
+    cout << "Решение" << N << " = " << fact(fact, N) << endl; 
+    return 0;
+}
